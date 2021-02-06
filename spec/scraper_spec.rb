e515d6cdb8bdbe35efcfb56ecff3e57fceed7475
franklin <franklinbenjamin032@gmail.com>
2021-02-06 18:45:43 -0400

    add tests units

---
+++ spec/scraper_spec.rb
@@ -0,0 +1,21 @@
+require './lib/scraper'
+require 'nokogiri'
+require 'open-uri'
+
+describe Scraper do
+  describe '.scraper' do
+    context 'the function is passed a good argument and is working correctly'
+    it 'return an array of hashes containing the jobs and their info' do
+      al = Scraper.new('engineer')
+      var = al.scraper
+      expect(var).to eql(al.scraper)
+    end
+
+    context 'the function is passed a bad argument and is working incorrectly'
+    it 'should return that no jobs were fiund' do
+      al = Scraper.new('frregtrge')
+      var = al.scraper
+      expect(var).to eql(al.scraper)
+    end
+  end
+end
